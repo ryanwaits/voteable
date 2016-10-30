6536cde1e79e1245f958e8f7bfd8d30775321499
Ryan Waits <ryan.waits@gmail.com>
2016-10-29 20:37:45 -0500

    add clearance

--- db/migrate/20161030012852_user.rb
+++ db/migrate/20161030012852_user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Migration[5.0]
   def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email, index: true
+      t.string :password
+
+      t.timestamps
+    end
   end
 end
