b53367e9d5179abe34d380094498d8019b633bc6
Ryan Waits <ryan.waits@gmail.com>
2016-10-31 13:21:47 -0500

    add routes for links // upvote and downvote// nested comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'links#index'
+  resources :links do
+    member do
+      put 'like', to: 'links#upvote'
+      put 'dislike', to: 'links#downvote'
+    end
+    resources :comments
+  end
 end
